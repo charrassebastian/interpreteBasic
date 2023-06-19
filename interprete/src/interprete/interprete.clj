2dcec822f0feb5edad47aace189e7f239bcb3d8e
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-06-19 14:21:28 -0300

    Agregados mas prints para depurar imprimir

--- interprete/src/interprete/interprete.clj
+++ interprete/src/interprete/interprete.clj
@@ -486,9 +486,17 @@
    (let [nueva (cons (conj [] (first lista-expr)) (rest lista-expr)),
          variable? #(or (variable-integer? %) (variable-float? %) (variable-string? %)),
          funcion? #(and (> (aridad %) 0) (not (operador? %))),
-         interc (reduce #(if (and (or (number? (last %1)) (string? (last %1)) (variable? (last %1)) (= (symbol ")") (last %1)))
-                                  (or (number? %2) (string? %2) (variable? %2) (funcion? %2) (= (symbol "(") %2)))
-                           (conj (conj %1 (symbol ";")) %2) (conj %1 %2)) nueva),
+         interc (reduce #(if (and (or (spy "number?" (number? (spy "por revisar 1" (last %1)))) 
+                                      (spy "string?" (string? (last %1))) 
+                                      (spy "variable?" (variable? (last %1)))
+                                      (spy "parentesis de cierre?" (= (symbol ")") (last %1))))
+                                  (or (spy "number?" (number? (spy "por revisar 2" %2))) 
+                                      (spy "string?" (string? %2)) 
+                                      (spy "variable?" (variable? %2)) 
+                                      (spy "funcion?" (funcion? %2)) 
+                                      (spy "parentesis de apertura?" (= (symbol "(") %2))))
+                           (spy "res rama condicion true" (conj (conj %1 (symbol ";")) %2)) 
+                           (spy "res rama condicion false" (conj %1 %2))) nueva),
          ex (partition-by #(= % (symbol ",t")) (desambiguar-comas interc)),
          expresiones (apply concat (map #(partition-by (fn [x] (= x (symbol ";"))) %) ex))]
      (spy "lista-expr" lista-expr)
